6d9a165f7402fef57d8fdb0b4fa677bbae60bc55
Amal <amalanilkumar282@gmail.com>
2025-07-27 21:21:26 +0530

    duo bg change

--- src/app/couple-duo/page.tsx
+++ src/app/couple-duo/page.tsx
@@ -44,7 +44,7 @@ export default function CoupleduoPage() {
         {/* Background */}
         <div className="absolute inset-0">
           <Image
-            src="/images/couple-duo.jpg"
+            src="/images/couple-duo-hero.jpg"
             alt={profile.name}
             fill
             className="object-cover"
