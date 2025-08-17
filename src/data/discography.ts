f2e5d3478a6c62b1f970bda264c1ea52aa74fe2b
Amal <amalanilkumar282@gmail.com>
2025-08-18 01:57:21 +0530

    track update

--- src/data/discography.ts
+++ src/data/discography.ts
@@ -84,6 +84,45 @@ export const discography: Track[] = [
     soundcloudTrackId: '1601264592',
     embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1601264592&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
   }
+  ,
+  {
+    id: 'commercial-sing-along-non-stop-mix',
+    title: 'Commercial Sing Along Non Stop Mix',
+    artist: 'DJ FELICITOUS',
+    genre: 'Commercial/Bollywood',
+    duration: 'N/A',
+    releaseDate: '2025-08-18',
+    albumArt: '/images/tracks/commercial-sing-along.jpg',
+    audioUrl: 'https://api.soundcloud.com/tracks/1879647267',
+    streamingLinks: {
+      soundcloud: 'https://soundcloud.com/dj-felicitous/commercial-sing-along-non-stop-mix-dj-felicitous',
+    },
+    description: 'A high-energy non-stop commercial sing-along mix by DJ Felicitous, perfect for parties and celebrations.',
+    featured: false,
+    type: 'live-set',
+    tags: ['Commercial', 'Bollywood', 'Party', 'Non Stop'],
+    soundcloudTrackId: '1879647267',
+    embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1879647267&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+  },
+  {
+    id: 'melodic-techno-1-hour-non-stop-mix',
+    title: 'Melodic Techno 1 Hour Non Stop Mix',
+    artist: 'DJ FELICITOUS',
+    genre: 'Melodic Techno',
+    duration: 'N/A',
+    releaseDate: '2025-08-18',
+    albumArt: '/images/tracks/melodic-techno-1hr.jpg',
+    audioUrl: 'https://api.soundcloud.com/tracks/1879650032',
+    streamingLinks: {
+      soundcloud: 'https://soundcloud.com/dj-felicitous-211226193/melodic-techno-1-hour-non-stop-mix-dj-felicitous',
+    },
+    description: 'A seamless 1-hour melodic techno journey by DJ Felicitous, blending deep grooves and uplifting melodies.',
+    featured: false,
+    type: 'live-set',
+    tags: ['Melodic Techno', 'Non Stop', 'Mix', 'Live Set'],
+    soundcloudTrackId: '1879650032',
+    embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1879650032&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+  }
 ];
 
 export const albums: Album[] = [];
